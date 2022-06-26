6cfb1b5a00d26e4369e5063cbc4b7a647235018b
Mohak Jain <mohakj500@gmail.com>
2022-06-26 10:04:08 +0530

    Hosting Configured

--- index.js
+++ index.js
@@ -6,15 +6,4 @@ client.on('ready', () => {
     console.log("Bot logged in as " + client.user.tag);
 });
 
-/*const { Client } = require('discord.js');
-const client = new Client({ intents: ['GUILD_MESSAGES', 'GUILDS'] });
-require("dotenv").config();
-
-client.on('ready', () => {
-    console.log("Bot Started");
-});
-
-client.login(process.env.TOKEN); 
-*/
-
 client.login(process.env.TOKEN);
