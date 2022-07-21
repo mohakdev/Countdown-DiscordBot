73b69ecd71e870c530026d52e5778c0cb08b209d
Mohak Jain <mohakj500@gmail.com>
2022-07-21 18:13:26 +0530

    message added

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ client.on('ready', () => {
     console.log("Bot logged in as " + client.user.tag);
 });
 
+client.on('message', () => {
+    if (message.content === '/Countdown') {
+        message.reply('pong');
+    }
+});
+
 client.login(process.env.TOKEN);
